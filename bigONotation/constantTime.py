f6d03d538f975fda1b4c669e785cf9fc814d02f3
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-04-09 20:17:45 -0300

    Improve question comments

--- bigONotation/constantTime.py
+++ bigONotation/constantTime.py
@@ -16,16 +16,15 @@ logFirstTwoCandiesFromList Worst Case Scenario (1000000 candies): O(1) Constant 
 """
 
 """
-What Big O Notation is used to represent Constant Time? 
+=== What Big O Notation is used to represent Constant Time? === 
 
 O(1)
 
-
-When an operation has Constant Time? 
+=== When an operation has Constant Time? === 
 
 Constant Time happens when the amount of operations remains the same (constant) even when the input size gets bigger 
 
-What happens when an algorithm has more than one operation with Constant Time? 
+=== What happens when an algorithm has more than one operation with Constant Time? ===  
 
 In the end, O(3) which can be the sum of 3 Constant Time operations,
 it's going to be summarize to O(1) because even if the input gets bigger, 
