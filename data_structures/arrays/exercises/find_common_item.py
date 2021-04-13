037bb84b8fff3d70b5fa07bd08a0c6b77372ab86
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-04-13 20:36:36 -0300

    Improve exercice solutions

--- data_structures/arrays/exercises/find_common_item.py
+++ data_structures/arrays/exercises/find_common_item.py
@@ -1,26 +1,32 @@
-# ==== KEY POINTS ====
-# Return boolean if it finds common items 
-# Input is a list of strings
-# It's possible to have duplicated items
-# Is there a change of the lists having more than 10 items? If so, is that a problem if we use a solution with quadratic complexity?
-# Which one is more important: Space Complexity or Time Complexity?
+""" 
+==== KEY POINTS ====
+- Return boolean if it finds common items 
+- Input is a list of strings
+- It's possible to have duplicated items
+- Is there a change of the lists having more than 10 items? If so, is that a problem if we use a solution with quadratic complexity?
+- Which one is more important: Space Complexity or Time Complexity?
+"""
 
 list_input_1 = ['a', 'b', 'c']
 list_input_2 = ['d', 'e', 'f', 'c']
 list_input_3 = ['g', 'y']
 
-# Brute Force Solution
-# Time Complexity: O(a*b)
-# Space Complexity: O(1)
+""" 
+==== BRUTE FORCE SOLUTION ====
+- Time Complexity: O(a*b)
+- Space Complexity: O(1)
+"""
 def find_common_item_1(list1, list2): 
   for item in list1:
     if item in list2: return True
 
   return False    
 
-# Better Solution regarding Time Complexity
-# Time Complexity: O(a + b)
-# Space Complexity: O(a)
+""" 
+==== SCALABLE SOLUTION ====
+- Time Complexity: O(a + b)
+- Space Complexity: O(a)
+"""
 def find_common_2(list1, list2): 
   list1_set = set(list1)
 
